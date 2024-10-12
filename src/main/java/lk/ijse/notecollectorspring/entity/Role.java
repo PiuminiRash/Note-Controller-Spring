6a9342ed77ded48ab47c081bbd0854637efabd05
PiuminiRash <piuminirashmika@gmail.com>
2024-10-12 13:43:55 +0530

    update

---
+++ src/main/java/lk/ijse/notecollectorspring/entity/Role.java
@@ -0,0 +1,5 @@
+package lk.ijse.notecollectorspring.entity;
+
+public enum Role {
+    ADMIN , USER
+}
--- src/main/java/lk/ijse/notecollectorspring/entity/impl/UserEntity.java
+++ src/main/java/lk/ijse/notecollectorspring/entity/impl/UserEntity.java
@@ -2,11 +2,15 @@ package lk.ijse.notecollectorspring.entity.impl;
 
 
 import jakarta.persistence.*;
+import lk.ijse.notecollectorspring.entity.Role;
 import lk.ijse.notecollectorspring.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -14,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class UserEntity implements SuperEntity {
+public class UserEntity implements SuperEntity , UserDetails {
     @Id
     private String userId;
     private String firstName;
@@ -26,4 +30,36 @@ public class UserEntity implements SuperEntity {
     private String profilePic;
     @OneToMany(mappedBy = "user")
     private List<NoteEntity> noteId;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
